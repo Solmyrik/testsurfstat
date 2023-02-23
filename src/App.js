be4aec9fb7ab364449773f68c60d347ccce2f0e0
C:\Users\Robik\Desktop\Проекты\React\solmlink <schedamian@gmail.com>
2023-02-23 18:14:57 +0300

    f

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  const ref = useRef();
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState([{ text: 'ok' }]);
+
+  useEffect(() => {
+    if (inputValue.length > 1) {
+      fetch(
+        `https://trending-searches.wb.ru/api?itemsPerPage=1000&offset=0&period=week&query=${inputValue}&sort=desc`,
+      )
+        .then((res) => res.json())
+        .then((data) => setItems(data.data.list));
+    }
+  }, [inputValue]);
+
+  const onSearch = () => {
+    setInputValue(ref.current.value);
+  };
+
+  console.log(items);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input">
+        <input ref={ref} type="text" />
+      </div>
+      <button onClick={onSearch}>Поиск</button>
+      <div>
+        {items &&
+          items.map((e) => {
+            return (
+              <p>
+                {e.text} {e.requestCount}
+              </p>
+            );
+          })}
+      </div>
     </div>
   );
 }
